826c8d77181b763e58816471534b676a44cbeffb
vpavlenko <cxielamiko@gmail.com>
2013-05-25 12:04:32 +0400

    more details to API

--- vitalik_api.py
+++ vitalik_api.py
@@ -10,16 +10,26 @@ class Male(Person):
 
 
 class Vitalik(Male):
-    '''Define basic API every handsome guy can rely on.  Some methods are
-    also available to girls.
+    '''
+    Define basic API every smart tender interesting handsome guy can
+    rely on.  Some methods are also available to girls.
     '''
 
-    def spend_the_night(self, date: datetime.date, location, caller: Person, 
-                        supper_proposal={('spaghetti', 'initiator'), 
+    def spend_the_night(self, date: datetime.date, location, caller: Person,
+                        supper_proposal={('spaghetti', 'initiator'),
                                          ('bolognese', 'Vitalik')}) -> bool:
+        '''
+        Consider famous meeting format: walking before, cooking or eating after,
+        chatting, hugging, kissing, maybe mutual masturbation with elements of
+        oral sex, sleeping together (better naked).  There is nothing new under
+        the sun.
+        '''
         pass  # hey, it's just a Python keyword
 
     def nearest_days_with_free_space(self, caller: Male) -> list:
+        pass
+
+    def nearest_days_with_free_time(self, caller: Person) -> list:
         pass
 
     def go_for_a_walk(self, date: datetime.date, caller: Person):
