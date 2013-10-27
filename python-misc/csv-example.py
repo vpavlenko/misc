950982d731b34e0fbde30d9f31dcb8ac184ed49f
vpavlenko <cxielamiko@gmail.com>
2013-10-27 22:12:48 +0400

    python write to csv example

---
+++ python-misc/csv-example.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import csv
+
+
+def write_to_csv(filename, table):
+    with open(filename, 'w') as output_file:
+        output_writer = csv.writer(output_file)
+        for row in table:
+            output_writer.writerow(row)
+
+
+def main():
+    table = []
+    for phi in range(0, 375, 15):
+        table.append([phi, phi ** 2])
+    write_to_csv('mytable.csv', table)
+
+
+if __name__ == '__main__':
+    main()
